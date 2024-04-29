be859fae5d18f0a1be490fb66230a7ea4814fe8b
oishee <oisheekhan90@gmail.com>
2024-04-30 02:40:47 +0600

    delete part complete

--- src/Pages/MyList.jsx
+++ src/Pages/MyList.jsx
@@ -6,14 +6,27 @@ import { Link } from "react-router-dom";
 const MyList = () => {
     const {user} = UseAuth() || {};
     const [items,setItem]=useState([])
+    const [control,setControl]=useState(false)
     useEffect(() => {
         fetch(`http://localhost:4000/myList/${user?.email}`)
         .then(res => res.json())
         .then(data => {
             setItem(data)
         })
-    },[])
+    },[user,control])
+ const handleDelete = (id) => {
+    fetch(`http://localhost:4000/delete/${id}`,{
+        method:"Delete",
 
+    })
+    .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                setControl(!control)
+            }
+        })
+
+ }
     return (
         <div className="container mx-auto mt-20 bg-[#faebd9] min-h-screen">
             <div className="font-ubuntu my-8 text-center space-y-6 mt-10 mb-10">
@@ -43,7 +56,7 @@ const MyList = () => {
                                 <Link to={`/update/${item._id}`}><button className="transition duration-1000 ease-in  cursor-pointer bg-[#5f2d70de]  hover:bg-[#4d205c] font-ubuntu text-white font-bold py-2 px-4 rounded">
                                     Update
                                 </button></Link>
-                                <button className="transition duration-1000 ease-in  cursor-pointer bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                <button onClick={()=>handleDelete(item._id)} className="transition duration-1000 ease-in  cursor-pointer bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                                     Delete
                                 </button>
                             </td>
