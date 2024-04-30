c47117b3792195864763fe9d1f69ea9771529b2b
oishee <oisheekhan90@gmail.com>
2024-04-30 14:40:03 +0600

    updated firebases config

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4V2JIDlyfgIdKxpguvmFU4bM0qLQPbvc",
-  authDomain: "tourism-management-websi-a73be.firebaseapp.com",
-  projectId: "tourism-management-websi-a73be",
-  storageBucket: "tourism-management-websi-a73be.appspot.com",
-  messagingSenderId: "52071572415",
-  appId: "1:52071572415:web:8de9bcde924d6f9133b70a"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
